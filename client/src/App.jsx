2b835a3cd1d52c2b32d5ec18538f9bb06a345036
izen1231 <izen1231@gmail.com>
2020-11-04 16:11:46 +0900

    update

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import './App.css';
+import './common.css'
+import {AdvancedTypescriptEditor} from "./components/AdvancedTypescriptEditor";
+
+class App extends Component {
+    render() {
+        return (
+            <AdvancedTypescriptEditor/>
+        );
+    }
+}
+
+export default App;
